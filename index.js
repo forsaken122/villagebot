ac6038728e60c003b1941c6bab19c49433fe5724
Pierluigi Ballatore <pierluigi@MacBook-Pro-di-Pierluigi.local>
2018-06-14 19:43:12 +0200

    bugfix

--- index.js
+++ index.js
@@ -3,13 +3,7 @@
 const Telegram = require('telegram-node-bot')
 const TelegramBaseController = Telegram.TelegramBaseController
 const TextCommand = Telegram.TextCommand
-const tg = new Telegram.Telegram(process.env.TELEGRAM_TOKEN, {
-    webhook: {
-      "url": "https://villagetorrentbot.herokuapp.com",
-      "port": 3000,
-      "host": "localhost"
-    }
-  })
+const tg = new Telegram.Telegram(process.env.TELEGRAM_TOKEN)
   //process.env.TELEGRAM_TOKEN)
 
 const querystring = require('querystring');
